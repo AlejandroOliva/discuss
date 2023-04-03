8d031d0b59c3ba10f5e3f9bb0a56511fd64082a1
Jandro Oliva <finaldownjandro@gmail.com>
2023-04-03 11:55:18 +0200

    Handling form errors

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,6 +2,7 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias Discuss.Topic
+  alias Discuss.Repo
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
@@ -10,7 +11,12 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
 
-    conn
+    case Repo.insert(changeset) do
+      {:ok, response} -> IO.inspect(response, label: ">>>>>>> Jandro OK")
+      {:error, changeset} -> 
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
